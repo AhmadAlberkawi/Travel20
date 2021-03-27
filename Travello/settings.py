b2aea6ee96d4b03b7969af8fce327c95e25a2974
AhmadAlberkawi <46059053+AhmadAlberkawi@users.noreply.github.com>
2021-03-27 18:06:34 +0100

    Update settings.py

--- Travello/settings.py
+++ Travello/settings.py
@@ -136,6 +136,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER="zaher.abb12@gmail.com"
-EMAIL_HOST_PASSWORD="0951225314.za"
+EMAIL_HOST_PASSWORD="***"
 EMAIL_USE_TLS=True
 #EMAIL_USE_SSL=False
